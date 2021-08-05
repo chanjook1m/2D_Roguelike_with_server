10c6e4c3fe9f77dd1d87b5ccac3257e108e4dc1e
1z3r091 <1z3r091@gmail.com>
2021-08-06 03:06:29 +0900

    first commit

---
+++ Server_1/main.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <ctime>
+#include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+#include "net.hpp"
+
+
+
+int main()
+{
+	static int id = 0;
+	std::cout << "Server running" << std::endl;
+
+	sf::TcpListener listener;
+	listener.setBlocking(false);
+	sf::SocketSelector selector;
+	bool done = false;
+
+	std::vector<sf::TcpSocket*> clients;
+	std::vector<net::Player> players;
+
+	listener.listen(5555);
+	selector.add(listener);
+
+	while (!done)
+	{
+		if (selector.wait(sf::seconds(2.0f)))
+		{
+			if (selector.isReady(listener))
+			{
+				sf::TcpSocket* socket = new sf::TcpSocket();
+
+				listener.accept(*socket);
+
+				sf::Packet packet;
+
+
+
+				if (socket->receive(packet) == sf::Socket::Done)
+				{
+					if (packet.getDataSize() > 0)
+					{
+						std::cout << "packet data: " << packet.getData() << std::endl;
+						std::cout << id << " has connected" << std::endl;
+						clients.push_back(socket);
+						selector.add(*socket);
+
+						sf::Packet packet;
+						net::Player newPlayer = net::Player();
+						newPlayer.id = id++;
+						players.push_back(newPlayer);
+						//newPlayer.hp = 80;
+						packet << newPlayer;
+
+						socket->send(packet);
+					}
+
+				}
+
+
+			}
+			else
+			{
+
+				/*while (true)
+				{*/
+
+				//}
+				for (int i = 0; i < clients.size(); i++)
+				{
+					if (selector.isReady(*clients[i]))
+					{
+
+						sf::Packet packet, sendPacket;
+						if (clients[i]->receive(packet) == sf::Socket::Done)
+						{
+							packet >> players[i];
+							std::cout << "id: " << players[i].id << " - pos :" << players[i].collisionRect_x << " , " << players[i].collisionRect_y << std::endl;
+							players[i].updated = true;
+							/*players[i].update();
+							sendPacket << players[i];
+							clients[i]->send(sendPacket);*/
+							/*std::string text;
+							packet >> text;
+							sendPacket << text;
+							for (int j = 0; j < clients.size(); j++)
+							{
+								if (i != j)
+								{
+									clients[j]->send(sendPacket);
+								}
+							}*/
+						}
+					}
+				}
+			}
+		}
+		/*else
+		{*/
+		//std::cout << players.size() << " and " << clients.size() << std::endl;
+		//double time = GetTickCount();
+		// UpdateGame();
+
+
+		for (size_t i = 0; i < players.size(); i++)
+		{
+			//if (players[i].shooted)
+			//{
+			std::cout << "shoot" << std::endl;
+			players[i].projectileUpdate();
+			//}
+			if (players[i].updated)
+			{
+
+				players[i].update();
+
+				players[i].updated = false;
+			}
+		}
+
+		for (size_t i = 0; i < clients.size(); i++)
+		{
+
+
+			sf::TcpSocket* client = clients[i];
+			client->setBlocking(false);
+			for (size_t j = 0; j < players.size(); j++)
+			{
+				sf::Packet packet;
+
+				packet << players[j];
+				if (client->send(packet) != sf::Socket::Done) {
+					std::cout << "[SERVER] could not send packet" << std::endl;
+				}
+				else
+				{
+					std::cout << "[SERVER] send to player " << players[j].id << std::endl;
+				}
+			}
+		}
+		//
+		//double deltaT = GetTickCount() - time;
+		//if (deltaT < 33)
+		//{
+		//	//Sleep(33 - deltaT);
+		//}
+	//}
+	}
+	for (std::vector<sf::TcpSocket*>::iterator it = clients.begin(); it != clients.end(); it++)
+		delete* it;
+
+	return 0;
+}
---
+++ Server_1/net.hpp
@@ -0,0 +1,175 @@
+#pragma once
+
+#include <SFML/Network.hpp>
+#include <SFML\Graphics.hpp>
+
+namespace net
+{
+	class Entity
+	{
+	public:
+		sf::RectangleShape collisionRect; // for collision
+		sf::Sprite sprite;
+		sf::Text text;
+
+		int spriteWidth = 0;
+		int spriteHeight = 0;
+
+		bool isAlive = true;
+		//bool isDeleted = false;
+
+		int x = 0;
+		int y = 0;
+	};
+
+	class Player : public Entity
+	{
+	public:
+		int id = 1;
+		int velocity = 2;
+		float attackDamage = 1.0f;
+		int powerUpLevel = 1;
+		int maxPowerUpLevel = 4;
+		int walkSpriteNumber = 0;
+		int direction = 0; // 1 = up, 2 = down, 3 = left, 4 = right
+		int hp = 100;
+		int maxHp = 100;
+		int score = 0;
+
+		bool canMoveUp = true;
+		bool canMoveDown = true;
+		bool canMoveLeft = true;
+		bool canMoveRight = true;
+
+		int collisionRect_x = 300;
+		int collisionRect_y = 400;
+
+		int temp_x = collisionRect_x;
+		int temp_y = collisionRect_y;
+
+
+		// projectile
+		int projectile_x = collisionRect_x;
+		int projectile_y = collisionRect_y;
+
+		int temp_projectile_x = collisionRect_x;
+		int temp_projectile_y = collisionRect_y;
+
+		int lifetime = 100;
+		int lifetimeCounter = 0;
+
+		bool updated = false;
+		bool shooted = false;
+		void update()
+		{
+			move();
+			//sprite.setPosition(collisionRect.getPosition());
+		}
+
+		void projectileUpdate()
+		{
+			if (direction == 1)
+			{
+				projectile_x = temp_projectile_x;
+				projectile_y = temp_projectile_y - velocity;
+				temp_projectile_y = projectile_y;
+			}
+
+			if (direction == 2 || direction == 0)
+			{
+				projectile_x = temp_projectile_x;
+				projectile_y = temp_projectile_y + velocity;
+				temp_projectile_y = projectile_y;
+			}
+
+			if (direction == 3)
+			{
+				projectile_y = temp_projectile_y;
+				projectile_x = temp_projectile_x - velocity;
+				temp_projectile_x = projectile_x;
+
+			}
+
+			if (direction == 4)
+			{
+				projectile_y = temp_projectile_y;
+				projectile_x = temp_projectile_x + velocity;
+				temp_projectile_x = projectile_x;
+			}
+
+			lifetimeCounter++;
+
+			if (lifetimeCounter >= lifetime)
+			{
+				lifetimeCounter = 0;
+				isAlive = false;
+				shooted = false;
+			}
+		}
+
+		void move()
+		{
+			if (direction == 1 && canMoveUp)
+			{
+				//collisionRect.move(0.f, -velocity);
+				//collisionRect_x = collisionRect.getPosition().x;
+				//collisionRect_y = collisionRect.getPosition().y;
+				collisionRect_x = temp_x;
+				collisionRect_y = temp_y - velocity;
+				temp_y = collisionRect_y;
+				std::cout << collisionRect.getPosition().y << std::endl;
+				std::cout << collisionRect_y << std::endl;
+
+			}
+			else if (direction == 2 && canMoveDown)
+			{
+				/*collisionRect.move(0.f, velocity);
+				collisionRect_x = collisionRect.getPosition().x;
+				collisionRect_y = collisionRect.getPosition().y;*/
+				collisionRect_x = temp_x;
+				collisionRect_y = temp_y + velocity;
+				temp_y = collisionRect_y;
+				std::cout << collisionRect.getPosition().y << std::endl;
+				std::cout << collisionRect_y << std::endl;
+			}
+			else if (direction == 3 && canMoveLeft)
+			{
+				//collisionRect.move(-velocity, 0.f);
+				collisionRect_x = temp_x - velocity;
+				temp_x = collisionRect_x;
+				collisionRect_y = temp_y;
+				//collisionRect_y = collisionRect.getPosition().y;
+				//std::cout << collisionRect_x << " and " << collisionRect_y << std::endl;
+			}
+			else if (direction == 4 && canMoveRight)
+			{
+				//collisionRect.move(velocity, 0.f);
+				collisionRect_x = temp_x + velocity;
+				temp_x = collisionRect_x;
+				collisionRect_y = temp_y;
+				//collisionRect_y = collisionRect.getPosition().y;
+				//std::cout << collisionRect_x << " and " << collisionRect_y << std::endl;
+			}
+			else
+			{
+				canMoveUp = true;
+				canMoveDown = true;
+				canMoveLeft = true;
+				canMoveRight = true;
+			}
+
+			//direction = 0;
+		}
+	};
+
+	sf::Packet& operator <<(sf::Packet& packet, const Player& m)
+	{
+		return packet << m.id << m.velocity << m.attackDamage << m.direction << m.x << m.y << m.hp << m.powerUpLevel << m.canMoveUp << m.canMoveDown << m.canMoveLeft << m.canMoveRight << m.isAlive << m.collisionRect_x << m.collisionRect_y << m.projectile_x << m.projectile_y << m.shooted;
+	}
+
+	sf::Packet& operator >>(sf::Packet& packet, Player& m)
+	{
+
+		return packet >> m.id >> m.velocity >> m.attackDamage >> m.direction >> m.x >> m.y >> m.hp >> m.powerUpLevel >> m.canMoveUp >> m.canMoveDown >> m.canMoveLeft >> m.canMoveRight >> m.isAlive >> m.collisionRect_x >> m.collisionRect_y >> m.projectile_x >> m.projectile_y >> m.shooted;
+	}
+}
